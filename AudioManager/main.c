304773414d55662bf31332cc2493e7fa06914e01
Nic <nicdevel@gmail.com>
2013-10-21 21:43:35 -0400

    Initial Commit

---
+++ AudioManager/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AudioManager
+//
+//  Created by Nicholaus Mollel on 10/21/13.
+//  Copyright 2013 Amherst. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
